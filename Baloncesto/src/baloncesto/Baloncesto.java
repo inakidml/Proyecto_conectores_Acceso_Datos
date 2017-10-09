9eb07d4d37f9947c456ed1d166f5dfafaeaaa57a
Unknown <jonlopezgarrido@gmail.com>
2017-10-09 21:54:23 +0200

    Auto stash before merge of "master" and "origin/master"
    Comienzo de la vista

--- Baloncesto/src/baloncesto/Baloncesto.java
+++ Baloncesto/src/baloncesto/Baloncesto.java
@@ -6,10 +6,15 @@
 package baloncesto;
 
 import baloncesto.modelo.Conector.Conector;
+import baloncesto.modelo.Equipo;
+import baloncesto.vista.vistaPrincipal;
 import com.mysql.jdbc.Connection;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Properties;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -20,11 +25,31 @@ public class Baloncesto extends Conector {
     /**
      * @param args the command line arguments
      */
+    
+    private static vistaPrincipal vP;
+    
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-        // TODO code application logic here
-        Connection con = Conector.getCon_mysql_jdbc();
+        // TODO code application logic here      
+        //Lanzamos la seleccion de los conectores con la Base de datos
         
-        con.close();
+        ArrayList<Equipo> arrL_equipos = new ArrayList<Equipo>();
         
+        Equipo eq = new Equipo(0, "Baskonia jj", 0, "", "", "");
+        arrL_equipos.add(eq);
+      
+                /*
+        arrL_equipos.add(mikelDB4O());
+        arrL_equipos.add(mikelMYSQL());
+        arrL_equipos.add(mikelSQL());
+        */
+        lanzarVistaPrincipal(arrL_equipos);
+        
+    }
+    
+    
+    public static void lanzarVistaPrincipal( ArrayList<Equipo> arrL_equipos ){               
+        Baloncesto.vP = new vistaPrincipal(arrL_equipos);
+        vP.setLocationRelativeTo(vP);
+        vP.setVisible(true);
     }
 }
