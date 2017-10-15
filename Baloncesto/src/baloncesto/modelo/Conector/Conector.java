1e4f650166dcc6e52595cb3b7c63c9ecb495b412
inakidml <inaki.diaz@ikasle.egibide.org>
2017-10-15 20:42:58 +0200

    switch para sistema operativo en conector db40

--- Baloncesto/src/baloncesto/modelo/Conector/Conector.java
+++ Baloncesto/src/baloncesto/modelo/Conector/Conector.java
@@ -5,7 +5,6 @@
  */
 package baloncesto.modelo.Conector;
 
-
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 import com.mysql.jdbc.Connection;
@@ -17,6 +16,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author 9fdam03
@@ -26,77 +26,84 @@ public abstract class Conector {
     protected static String getAbsoluteResourceName(String resourceName) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     protected static com.mysql.jdbc.Connection con_mysql_jdbc;
     protected static java.sql.Connection con_sql;
-    protected static ObjectContainer db_4o;    
-    
+    protected static ObjectContainer db_4o;
+
     private static Properties config;
-    
+
     protected static Connection getCon_mysql_jdbc() throws ClassNotFoundException, SQLException {
-        
+
         try {
             Conector.setConfig(Conector.getConfig());
         } catch (IOException ex) {
             Logger.getLogger(Conector.class.getName()).log(Level.SEVERE, null, ex);
         }
-                
-        Class.forName("com.mysql.jdbc.Driver");
-        
-//        System.out.println( config.getProperty("sql.server"));
 
-        Conector.con_mysql_jdbc = (Connection) DriverManager.getConnection("jdbc:mysql://"+ config.getProperty("mysql.server") +"/" +config.getProperty("mysql.bd") + "",config.getProperty("mysql.user"), config.getProperty("mysql.pass")); //pruebas","root", "usbw");   
-        
+        Class.forName("com.mysql.jdbc.Driver");
+
+//        System.out.println( config.getProperty("sql.server"));
+        Conector.con_mysql_jdbc = (Connection) DriverManager.getConnection("jdbc:mysql://" + config.getProperty("mysql.server") + "/" + config.getProperty("mysql.bd") + "", config.getProperty("mysql.user"), config.getProperty("mysql.pass")); //pruebas","root", "usbw");   
+
         return con_mysql_jdbc;
-        
+
     }
 
     protected static java.sql.Connection getCon_sql() throws ClassNotFoundException, SQLException {
-        
+
         try {
             Conector.setConfig(Conector.getConfig());
         } catch (IOException ex) {
             Logger.getLogger(Conector.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        
-        Conector.con_sql = DriverManager.getConnection("jdbc:sqlserver://" + config.getProperty("sql.server") + ":" + config.getProperty("sql.port")  + ";databaseName=" + config.getProperty("sql.bd")  + ";user=" + config.getProperty("sql.user") + ";password=");
-        
+
+        Conector.con_sql = DriverManager.getConnection("jdbc:sqlserver://" + config.getProperty("sql.server") + ":" + config.getProperty("sql.port") + ";databaseName=" + config.getProperty("sql.bd") + ";user=" + config.getProperty("sql.user") + ";password=");
+
         return con_sql;
-        
+
     }
 
     protected static ObjectContainer getDb_4o() {
-        
+
         try {
             Conector.setConfig(Conector.getConfig());
         } catch (IOException ex) {
             Logger.getLogger(Conector.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        String BDBaloncesto = config.getProperty("db4o");
-        
-        Conector.db_4o = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), BDBaloncesto);
-            
-        return db_4o;
-        
-    }       
 
-    protected static Properties getConfig() throws FileNotFoundException, IOException {          
+        String BDBaloncesto = config.getProperty("db4o");
+
+        Conector.db_4o = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), BDBaloncesto);
+
+        return db_4o;
+
+    }
+
+    protected static Properties getConfig() throws FileNotFoundException, IOException {
+        System.out.println(System.getProperty("os.name"));
         
-        FileInputStream file = new FileInputStream("src\\baloncesto\\modelo\\Conector\\config.properties");
-        
+        FileInputStream file = null;
+        switch (System.getProperty("os.name")) {
+            case "Linux":
+                file = new FileInputStream("src/baloncesto/modelo/Conector/config.properties");
+                break;
+            default:
+                file = new FileInputStream("src\\baloncesto\\modelo\\Conector\\config.properties");
+        }
+
         Properties config_ = new Properties();
-        
+
         config_.load(file);
-        
+
         return config_;
-        
-    }    
+
+    }
 
     protected static void setConfig(Properties config) {
         Conector.config = config;
     }
-       
- }
+
+}
